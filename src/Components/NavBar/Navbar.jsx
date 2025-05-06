d49e270e08b3e04d85d524d89363c60cec7b3aab
Duc Son Nguyen <duc.son.nguyen48@gmail.com>
2025-05-06 03:08:18 +0200

    menu mob

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.svg';
-import underline from '../../assets/nav_underline.svg';  
+import underline from '../../assets/nav_underline.svg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 import menu_open from '../../assets/menu_open.svg';
@@ -9,13 +9,22 @@ import menu_close from '../../assets/menu_close.svg';
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
-  
-  return ( 
+  const menuRef = useRef();
+
+  const openMenu = () => {
+    menuRef.current.style.right="-350px";
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right="0";
+  }
+  return (
     <div className='navbar'>
-      <img src={logo} alt=''  />
-     { <img src={menu_open} alt='' className='nav-mob-open' />}
-      <ul className='nav-menu'>
-      <img src={menu_close} alt='' className= 'nav-mob-close'/>
+      <img src={logo} alt='' />
+
+      <img src={menu_open} onClick={openMenu} alt='' className='nav-mob-open' />
+
+      <ul ref={menuRef} className='nav-menu'>
+        <img src={menu_close} onClick= {closeMenu} alt='' className='nav-mob-close' />
         <li>
           <AnchorLink className='anchor-link' href='#home'>
             <p onClick={() => setMenu("home")}>Home</p>
@@ -48,9 +57,9 @@ const Navbar = () => {
         </li>
       </ul>
       <div className='nav-connect'>
-      <AnchorLink className='anchor-link' offset={50} href='#contact'>
-      Connectons nous !
-      </AnchorLink>
+        <AnchorLink className='anchor-link' offset={50} href='#contact'>
+          Connectons nous !
+        </AnchorLink>
       </div>
     </div>
   )
